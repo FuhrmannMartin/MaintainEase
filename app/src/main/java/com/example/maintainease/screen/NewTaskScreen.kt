48191cbff001a558e29c47bd80ff6af57a8c04e6
Roman Schuller <roman.schuller@gmail.com>
2024-05-26 18:45:09 +0200

    erste Textfields mit Buttons eingefügt, Cancel Button funktioniert

--- app/src/main/java/com/example/maintainease/screen/NewTaskScreen.kt
+++ app/src/main/java/com/example/maintainease/screen/NewTaskScreen.kt
@@ -1,14 +1,27 @@
 package com.example.maintainease.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.maintainease.data.InjectorUtils
@@ -28,14 +41,36 @@ fun NewTaskScreen(
 
     Scaffold(
         topBar = {
-            SimpleTopAppBar(title = "MaintainEase")
+            SimpleTopAppBar(title = "Add a new Maintenance Task")
         },
         bottomBar = {
             SimpleBottomAppBar(navController = navController)
         },
     ) { innerPadding ->
         Column(modifier = Modifier.padding(innerPadding)) {
-            Text(text = "PLACEHOLDER")
+
+            var text by remember {
+                mutableStateOf(TextFieldValue(""))
+            }
+            var textLocation by remember {
+                mutableStateOf(TextFieldValue(""))
+            }
+
+            OutlinedTextField(modifier = Modifier.padding(all = 15.dp).height(200.dp).fillMaxWidth(), value = text,  onValueChange = { newText -> text = newText}, label = { Text(text = "Description: ")} )
+
+            OutlinedTextField(modifier = Modifier.padding(all = 15.dp).height(100.dp).fillMaxWidth(), value = textLocation,  onValueChange = { newTextLocation -> textLocation = newTextLocation}, label = { Text(text = "Location: ")} )
+        //   Text(text = "PLACEHOLDER")
+
+            Row(modifier = Modifier.fillMaxWidth().padding(30.dp)) {
+                OutlinedButton(onClick = { /*TODO*/ }) {
+                    Text("Submit")
+                }
+
+                Spacer(modifier = Modifier.weight(1f))
+                OutlinedButton(onClick = { navController.popBackStack() }) {
+                    Text("Cancel")
+                }
+            }
         }
     }
 }
